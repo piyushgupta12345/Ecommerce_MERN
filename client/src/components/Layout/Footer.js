147024743af1917ae42526d70fa5067191ff3f57
Piyush Gupta <124340622+piyushgupta12345@users.noreply.github.com>
2023-10-09 22:06:49 +0530

    react setup

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='bg-dark text-light p-3'>
+      <h1 className="text-center">All Right Reserved &copy; Techinfoyt</h1>
+    </div>
+  )
+}
+
+export default Footer
