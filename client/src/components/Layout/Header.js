b4707bce1017fab4687a0b88e7d9a3423a27c0ea
Piyush Gupta <124340622+piyushgupta12345@users.noreply.github.com>
2023-10-10 16:57:43 +0530

    modified files

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <div>
-      Header
-    </div>
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <Link className="navbar-brand" to="/">🛒 Ecommerce App</Link>
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/register">Register</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/category">Category</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/login">Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/cart">Cart (0)</Link>
+              </li>
+            </ul>
+
+          </div>
+        </div>
+      </nav>
+    </>
   )
 }
 
--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import { Helmet } from "react-helmet";
 
-function Layout({children}) {
+function Layout({ children, title, description, keywords, author }) {
   return (
     <div>
-      <Header/>
-      <main style={{ minHeight: "80vh" }}>{children}</main>
-      <Footer/>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content={author} />
+        <title>{title}</title>
+      </Helmet>
+      <Header />
+      <main style={{ minHeight: "70vh" }}>{children}</main>
+      <Footer />
     </div>
   )
+}
+
+Layout.defaultProps = {
+  title:"Ecommerce app - shop now",
+  description: 'mern stack project',
+  keywords: 'mern,react,node,mongodb',
+  author:'Piyush Gupta'
 }
 
 export default Layout
